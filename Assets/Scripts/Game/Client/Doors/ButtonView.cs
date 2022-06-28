cde0429aff1ed3d85bf796f8415a72bc4f96cd2e
r.malinovskiy <r.malinovskiy@t-point.dev>
2022-06-28 18:05:03 +0300

    Представление кнопки

---
+++ Assets/Scripts/Game/Client/Doors/ButtonView.cs
@@ -0,0 +1,19 @@
+using Game.Core.Doors;
+using UnityEngine;
+
+namespace Game.Client
+{
+	public class ButtonView : BaseEntityView
+	{
+		[SerializeField] private DoorColor _color;
+		[SerializeField, Min(0f)] private float _radius = 0.5f;
+		
+		protected override void OnInitialized()
+		{
+			var entity = World.NewEntity();
+			ref var button = ref World.GetPool<FloorButton>().Add(entity);
+			button.Color = _color;
+			button.Radius = _radius;
+		}
+	}
+}
